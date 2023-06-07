504097cd12c0151daaea9ed8ae7706a60d5ec0eb
Jeff Scott <jpscott@ranken.edu>
2023-06-07 11:30:28 -0500

    Array Lookup Problem Done In Class W 6/7/2023

---
+++ C#ArrayLookup/C#ArrayLookup/frmArrayLookup.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace C_ArrayLookup
+{
+    partial class frmArrayLookup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSearchTerm = new System.Windows.Forms.Label();
+            this.lblEmployeeID = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblDepartment = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnSearchepartment = new System.Windows.Forms.Button();
+            this.btnSearchByID = new System.Windows.Forms.Button();
+            this.btnSearchFirstName = new System.Windows.Forms.Button();
+            this.btnSearchLastName = new System.Windows.Forms.Button();
+            this.txtSearchTerm = new System.Windows.Forms.TextBox();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtDepartment = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblSearchTerm
+            // 
+            this.lblSearchTerm.BackColor = System.Drawing.Color.Blue;
+            this.lblSearchTerm.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearchTerm.ForeColor = System.Drawing.Color.White;
+            this.lblSearchTerm.Location = new System.Drawing.Point(69, 59);
+            this.lblSearchTerm.Name = "lblSearchTerm";
+            this.lblSearchTerm.Size = new System.Drawing.Size(636, 66);
+            this.lblSearchTerm.TabIndex = 11;
+            this.lblSearchTerm.Text = "Enter ID, First Name, Last Name, or Department";
+            this.lblSearchTerm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblEmployeeID
+            // 
+            this.lblEmployeeID.BackColor = System.Drawing.Color.Blue;
+            this.lblEmployeeID.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmployeeID.ForeColor = System.Drawing.Color.White;
+            this.lblEmployeeID.Location = new System.Drawing.Point(69, 227);
+            this.lblEmployeeID.Name = "lblEmployeeID";
+            this.lblEmployeeID.Size = new System.Drawing.Size(358, 44);
+            this.lblEmployeeID.TabIndex = 12;
+            this.lblEmployeeID.Text = "Employee ID:";
+            this.lblEmployeeID.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.BackColor = System.Drawing.Color.Blue;
+            this.lblFirstName.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstName.ForeColor = System.Drawing.Color.White;
+            this.lblFirstName.Location = new System.Drawing.Point(69, 321);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(358, 44);
+            this.lblFirstName.TabIndex = 13;
+            this.lblFirstName.Text = "First Name:";
+            this.lblFirstName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.BackColor = System.Drawing.Color.Blue;
+            this.lblLastName.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.ForeColor = System.Drawing.Color.White;
+            this.lblLastName.Location = new System.Drawing.Point(69, 420);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(358, 41);
+            this.lblLastName.TabIndex = 14;
+            this.lblLastName.Text = "Last Name:";
+            this.lblLastName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDepartment
+            // 
+            this.lblDepartment.BackColor = System.Drawing.Color.Blue;
+            this.lblDepartment.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepartment.ForeColor = System.Drawing.Color.White;
+            this.lblDepartment.Location = new System.Drawing.Point(69, 512);
+            this.lblDepartment.Name = "lblDepartment";
+            this.lblDepartment.Size = new System.Drawing.Size(358, 41);
+            this.lblDepartment.TabIndex = 15;
+            this.lblDepartment.Text = "Department:";
+            this.lblDepartment.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClear
+            // 
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClear.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(72, 605);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(263, 70);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(449, 605);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(263, 70);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnSearchepartment
+            // 
+            this.btnSearchepartment.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearchepartment.Location = new System.Drawing.Point(872, 605);
+            this.btnSearchepartment.Name = "btnSearchepartment";
+            this.btnSearchepartment.Size = new System.Drawing.Size(263, 70);
+            this.btnSearchepartment.TabIndex = 4;
+            this.btnSearchepartment.Text = "Search Dept.";
+            this.btnSearchepartment.UseVisualStyleBackColor = true;
+            this.btnSearchepartment.Click += new System.EventHandler(this.btnSearchepartment_Click);
+            // 
+            // btnSearchByID
+            // 
+            this.btnSearchByID.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearchByID.Location = new System.Drawing.Point(872, 58);
+            this.btnSearchByID.Name = "btnSearchByID";
+            this.btnSearchByID.Size = new System.Drawing.Size(263, 70);
+            this.btnSearchByID.TabIndex = 1;
+            this.btnSearchByID.Text = "Search By ID";
+            this.btnSearchByID.UseVisualStyleBackColor = true;
+            this.btnSearchByID.Click += new System.EventHandler(this.btnSearchByID_Click);
+            // 
+            // btnSearchFirstName
+            // 
+            this.btnSearchFirstName.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearchFirstName.Location = new System.Drawing.Point(872, 256);
+            this.btnSearchFirstName.Name = "btnSearchFirstName";
+            this.btnSearchFirstName.Size = new System.Drawing.Size(263, 70);
+            this.btnSearchFirstName.TabIndex = 2;
+            this.btnSearchFirstName.Text = "Search First Name";
+            this.btnSearchFirstName.UseVisualStyleBackColor = true;
+            this.btnSearchFirstName.Click += new System.EventHandler(this.btnSearchFirstName_Click);
+            // 
+            // btnSearchLastName
+            // 
+            this.btnSearchLastName.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearchLastName.Location = new System.Drawing.Point(872, 448);
+            this.btnSearchLastName.Name = "btnSearchLastName";
+            this.btnSearchLastName.Size = new System.Drawing.Size(263, 70);
+            this.btnSearchLastName.TabIndex = 3;
+            this.btnSearchLastName.Text = "Search Last Name";
+            this.btnSearchLastName.UseVisualStyleBackColor = true;
+            this.btnSearchLastName.Click += new System.EventHandler(this.btnSearchLastName_Click);
+            // 
+            // txtSearchTerm
+            // 
+            this.txtSearchTerm.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearchTerm.Location = new System.Drawing.Point(72, 152);
+            this.txtSearchTerm.Name = "txtSearchTerm";
+            this.txtSearchTerm.Size = new System.Drawing.Size(633, 44);
+            this.txtSearchTerm.TabIndex = 0;
+            this.txtSearchTerm.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.txtEmployeeID.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmployeeID.ForeColor = System.Drawing.Color.White;
+            this.txtEmployeeID.Location = new System.Drawing.Point(467, 227);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.ReadOnly = true;
+            this.txtEmployeeID.Size = new System.Drawing.Size(325, 44);
+            this.txtEmployeeID.TabIndex = 7;
+            this.txtEmployeeID.Tag = "employeeID";
+            this.txtEmployeeID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.txtFirstName.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.ForeColor = System.Drawing.Color.White;
+            this.txtFirstName.Location = new System.Drawing.Point(467, 321);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.ReadOnly = true;
+            this.txtFirstName.Size = new System.Drawing.Size(325, 44);
+            this.txtFirstName.TabIndex = 8;
+            this.txtFirstName.Tag = "firstName";
+            this.txtFirstName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.txtLastName.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.ForeColor = System.Drawing.Color.White;
+            this.txtLastName.Location = new System.Drawing.Point(467, 417);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.ReadOnly = true;
+            this.txtLastName.Size = new System.Drawing.Size(325, 44);
+            this.txtLastName.TabIndex = 9;
+            this.txtLastName.Tag = "lastName";
+            this.txtLastName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtDepartment
+            // 
+            this.txtDepartment.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.txtDepartment.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDepartment.ForeColor = System.Drawing.Color.White;
+            this.txtDepartment.Location = new System.Drawing.Point(467, 509);
+            this.txtDepartment.Name = "txtDepartment";
+            this.txtDepartment.ReadOnly = true;
+            this.txtDepartment.Size = new System.Drawing.Size(325, 44);
+            this.txtDepartment.TabIndex = 10;
+            this.txtDepartment.Tag = "department";
+            this.txtDepartment.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // frmArrayLookup
+            // 
+            this.AcceptButton = this.btnSearchByID;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.CancelButton = this.btnClear;
+            this.ClientSize = new System.Drawing.Size(1177, 704);
+            this.Controls.Add(this.txtDepartment);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Controls.Add(this.txtSearchTerm);
+            this.Controls.Add(this.btnSearchLastName);
+            this.Controls.Add(this.btnSearchFirstName);
+            this.Controls.Add(this.btnSearchByID);
+            this.Controls.Add(this.btnSearchepartment);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblDepartment);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblEmployeeID);
+            this.Controls.Add(this.lblSearchTerm);
+            this.Name = "frmArrayLookup";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chapters 7 - 8 Pretest - Problem #1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSearchTerm;
+        private System.Windows.Forms.Label lblEmployeeID;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblDepartment;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnSearchepartment;
+        private System.Windows.Forms.Button btnSearchByID;
+        private System.Windows.Forms.Button btnSearchFirstName;
+        private System.Windows.Forms.Button btnSearchLastName;
+        private System.Windows.Forms.TextBox txtSearchTerm;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtDepartment;
+    }
+}
+
---
+++ C#ArrayLookup/C#ArrayLookup/frmArrayLookup.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Windows.Forms;
+
+/*
+ *  Write a GUI application that looks up employee 
+ *  information by ID, first name, last name, or department:
+ *
+ *  The program must store this information in four parallel 
+ *  arrays and use appropriate loops. Here is the data:
+ *
+ID:		      First Name	     Last Name			 Department
+----------------------------------------------------------------------------------------------
+1347		Scott 			    Gray			    Information Technology
+2698		Greg 			    Green		        Web Programming
+1444		Brian 		        Black			    Networking
+1987		Mary 			    Brown		        Web Programming
+3421		Joe 			    Barnes		        Networking
+7860		Gail 			    White			    Information Technology
+5590		Karen 		        Clark			    Database Programming
+6329		Mike			    King			    Networking
+8701		Barb			    Jones		        Web Programming
+5513		Cory			    Kent			    Database Programming
+
+
+The GUI app should include the following 4 arrays:
+
+int[] ids;
+string[] firstNames;
+string[] lastNames;
+string[] departments;
+ */
+
+namespace C_ArrayLookup
+{
+    public partial class frmArrayLookup : Form
+    {
+        public frmArrayLookup()
+        {
+            InitializeComponent();
+        }
+
+        //  Declare and initialize program constants
+        const int MINEMPLOYEEID = 1000;
+        const int MAXEMPLOYEEID = 9999;
+
+        //  Declare and initialize global arrays
+        int[] ids =
+        {
+            1347, 2698, 1444, 1987, 3421,
+            7860, 5590, 6329, 8701, 5513
+        };
+
+        string[] firstNames =
+        {
+            "Scott", "Greg", "Brian", "Mary", "Joe",
+            "Gail", "Karen", "Mike",  "Barb", "Cory"
+        };
+
+        string[] lastNames =
+        {
+            "Gray",  "Green", "Black", "Brown", "Barnes",
+            "White", "Clark", "King",  "Jones", "Kent"
+        };
+
+        string[] departments =
+        {
+            "Information Technology", "Web Programming",
+            "Networking",             "Web Programming",
+            "Networking",             "Information Technology",
+            "Database Programming",   "Networking",
+            "Web Programming",        "Database Programming"
+        };
+
+        private void btnSearchByID_Click(object sender, EventArgs e)
+        {
+            SearchForInputtedID();
+        }
+
+        private void SearchForInputtedID()
+        {
+            string term = txtSearchTerm.Text;
+
+            //  Validate the presence of text in txtSearchTerm
+            string errMsg = IsPresent(term, "employeeID", txtSearchTerm);
+
+            //  No text in txtSearchTerm so show error
+            DisplayTheErrorMessage(errMsg);
+
+            //  Validate that the text in txtSearchTerm is an integer
+            errMsg = IsInt32(term, "employeeID", txtSearchTerm);
+
+            //  No integer value in txtSearchTerm so show error
+            DisplayTheErrorMessage(errMsg);
+
+            //  Validate that the int in txtSearchTerm is in range
+            errMsg = IsWithinRange(term, "employeeID",
+                                   MINEMPLOYEEID, MAXEMPLOYEEID,
+                                   txtSearchTerm);
+
+            //  No integer value in txtSearchTerm so show error
+            DisplayTheErrorMessage(errMsg);
+
+            //  txtSearchTerm.Text holds a valid int value (1000 - 9999)
+            for (int lcv = 0; lcv < ids.Length; lcv++)
+            {
+                //  If the current id is in the ids array
+                if (ids[lcv].ToString().Contains(term))
+                {
+                    //  Show the data for that employee
+                    ShowTheData(lcv);
+                    return;
+                }
+            }
+
+            //  There was no match, i.e., no employee with that ID
+            //  Show ERROR in each of the 4 textboxes
+            ShowNoData();
+        }
+
+        private void btnSearchFirstName_Click(object sender, EventArgs e)
+        {
+            SearchForInputtedFirstName();
+        }
+
+        private void SearchForInputtedFirstName()
+        {
+            string term = txtSearchTerm.Text;
+
+            //  Validate the presence of text in txtSearchTerm
+            string errMsg = IsPresent(term, "firstName", txtSearchTerm);
+
+            //  No text in txtSearchTerm so show error
+            DisplayTheErrorMessage(errMsg);
+
+            //  txtSearchTerm.Text holds a valid first name
+            for (int lcv = 0; lcv < firstNames.Length; lcv++)
+            {
+                //  If the current id is in the ids array
+                if (firstNames[lcv].ToString().ToUpper().Contains(term.ToString().ToUpper()))
+                {
+                    //  Show the data for that employee
+                    ShowTheData(lcv);
+                    return;
+                }
+            }
+
+            //  There was no match, i.e., no employee with that first name
+            //  Show ERROR in each of the 4 textboxes
+            ShowNoData();
+        }
+
+        private void btnSearchLastName_Click(object sender, EventArgs e)
+        {
+            SearchForInputtedLastName();
+        }
+
+        private void SearchForInputtedLastName()
+        {
+            string term = txtSearchTerm.Text;
+
+            //  Validate the presence of text in txtSearchTerm
+            string errMsg = IsPresent(term, "lastName", txtSearchTerm);
+
+            //  No text in txtSearchTerm so show error
+            DisplayTheErrorMessage(errMsg);
+
+            //  txtSearchTerm.Text holds a valid last name
+            for (int lcv = 0; lcv < lastNames.Length; lcv++)
+            {
+                //  If the current id is in the ids array
+                if (lastNames[lcv].ToString().ToUpper().Contains(term.ToString().ToUpper()))
+                {
+                    //  Show the data for that employee
+                    ShowTheData(lcv);
+                    return;
+                }
+            }
+
+            //  There was no match, i.e., no employee with that last name
+            //  Show ERROR in each of the 4 textboxes
+            ShowNoData();
+        }
+
+        private void btnSearchepartment_Click(object sender, EventArgs e)
+        {
+            SearchForInputtedDepartment();
+        }
+
+        private void SearchForInputtedDepartment()
+        {
+            string term = txtSearchTerm.Text;
+
+            //  Validate the presence of text in txtSearchTerm
+            string errMsg = IsPresent(term, "department", txtSearchTerm);
+
+            //  No text in txtSearchTerm so show error
+            DisplayTheErrorMessage(errMsg);
+
+            //  txtSearchTerm.Text holds a valid department
+            for (int lcv = 0; lcv < lastNames.Length; lcv++)
+            {
+                //  If the current id is in the ids array
+                if (departments[lcv].ToString().ToUpper().Contains(term.ToString().ToUpper()))
+                {
+                    //  Show the data for that employee
+                    ShowTheData(lcv);
+                    return;
+                }
+            }
+
+            //  There was no match, i.e., no employee with that department
+            //  Show ERROR in each of the 4 textboxes
+            ShowNoData();
+        }
+
+        private void DisplayTheErrorMessage(string errMsg)
+        {
+            //  No text in txtSearchTerm so show error
+            if (errMsg != "")
+            {
+                ShowMessage(errMsg, "ERROR");
+            }
+        }
+
+        //  Validate the presence or absence of control textbox text
+        private string IsPresent(string value, string name, Control ctrl)
+        {
+            string msg = "";
+
+            if (value == "")
+            {
+                msg = name + " is a required field.\n";
+                ClearAndSetFocusToCorrectControl(ctrl);
+            }
+
+            return msg;
+        }
+
+        //  Validate the control textbox text is an integer
+        private string IsInt32(string value, string name, Control ctrl)
+        {
+            string msg = "";
+
+            if (!Int32.TryParse(value, out _))
+            {
+                msg = name + " must be a valid integer value.\n";
+                ClearAndSetFocusToCorrectControl(ctrl);
+            }
+
+            return msg;
+        }
+
+        //  Validate the control textbox text is between a min and max
+        private string IsWithinRange(string value, string name, 
+                                     int min, int max, Control ctrl)
+        {
+            string msg = "";
+            int    id;
+
+            if (Int32.TryParse(value, out id))
+            {
+                if ((id < min) || (id > max))
+                {
+                    msg = name + " must be between " + min + 
+                          " and " + max + "\n";
+                    ClearAndSetFocusToCorrectControl(ctrl);
+                }
+            }
+
+            return msg;
+        }
+
+        private void ClearAndSetFocusToCorrectControl(Control ctrl)
+        {
+            ctrl.Text = "";
+            ctrl.Focus();
+        }
+
+        private void ShowTheData(int lcv)
+        {
+            txtEmployeeID.Text  = ids[lcv].ToString();
+            txtFirstName.Text   = firstNames[lcv];
+            txtLastName.Text    = lastNames[lcv];
+            txtDepartment.Text  = departments[lcv];
+        }
+
+        private void ShowNoData()
+        {
+            txtEmployeeID.Text  = "ERROR";
+            txtFirstName.Text   = "ERROR";
+            txtLastName.Text    = "ERROR";
+            txtDepartment.Text  = "ERROR";
+            txtSearchTerm.Text  = "";
+            txtSearchTerm.Focus();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+        }
+
+        private void ClearAll() 
+        {
+            txtSearchTerm.Text = "";
+            txtEmployeeID.Text = "";
+            txtFirstName.Text  = "";
+            txtLastName.Text   = "";
+            txtDepartment.Text = "";
+            txtSearchTerm.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            ExitProgramOrNot();
+        }
+
+        private void ExitProgramOrNot()
+        {
+            DialogResult dialog = MessageBox.Show(
+            "Do You Really Want To Exit The Program?",
+            "EXIT NOW?",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Question);
+
+            if (dialog == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        //  Custom Error Message MessageBoxes
+        private void ShowMessage(string msg, string title)
+        {
+            MessageBox.Show(msg, title,
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+        }
+    }
+}
